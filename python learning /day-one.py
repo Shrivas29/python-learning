cd977c408cbc8d66dd5424a5a217e17201e1c76c
Shrivas <Shrivasvm@gmail.com>
2024-03-26 21:39:29 +0530

    test commit

---
+++ python learning /day-one.py
@@ -0,0 +1,25 @@
+#print fuunction is a key word used to print a line or a variable 
+
+print("hello world\nhello world")
+
+# \n is used to make the words come in coming lines rather than at the same line 
+
+print("hello"+" "+"shrivas")
+
+#input function is used to create a interface between the person and the code itself 
+
+input("how was your day")
+
+#the input function can be used in combination with print function 
+
+print("that great to hear"+input("how was your day"))
+
+#variables are enetites which contain or represent a value. infinite amount of value can be added.
+
+name = input("what is your name")
+
+print(name)
+
+#variable can be also varied. we can change the value of an variable any point of time 
+
+name = input("a")
