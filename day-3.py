b524732363801151b69706d3e458be5205ce439b
Shrivas <Shrivasvm@gmail.com>
2024-03-30 20:19:04 +0530

    day-3.py

---
+++ day-3.py
@@ -0,0 +1,32 @@
+
+#Variables, expressions and statements 
+#values and types
+
+#integers 
+
+name = 6
+print(type(name))
+
+#strings 
+
+name_1 = "shrivas"
+print(type(name_1))
+
+#data types
+#Float
+
+pi = 3.14
+print(type(pi))
+
+#integers
+#strings 
+
+#variabels 
+#it is an entity which has value to it 
+
+num_char = 69
+print(num_char)
+
+
+x = 5 
+print( x + 1)
