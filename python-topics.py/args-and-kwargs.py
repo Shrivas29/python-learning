59c52ce1dbe269a6a24aa4764a0491b58af92601
Shrivas <Shrivasvm@gmail.com>
2024-04-16 13:18:50 +0530

    python-learning.py

---
+++ python-topics.py/args-and-kwargs.py
@@ -0,0 +1,36 @@
+#args allows you to pass multiple non keyword arguments 
+#example 1
+def add(*args):
+    total = 0
+    for arg in args:
+        total += arg
+    return total
+
+print(add(1,3))
+
+#example 2
+def name(*args):
+    for arg in args:
+        print(arg,end = " ")
+
+name("shrivas","venkatasami","murali")
+#kwargs
+#allows you to pass many key wrod arguments
+
+def location(**kwargs):
+    for key in kwargs.keys:
+        print(key)
+location(appartement = "SGV",BLOCK = "c2")
+
+#palindrome
+def vari():
+    n = input("enter a string ")
+    if n == n[::-1]:
+        print("it is a palindrom")
+    else:
+        print("its not a plindrome")
+
+
+
+
+    
