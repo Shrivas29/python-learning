ed78517a9f43d89f93f119c222b8168200476e60
Shrivas <Shrivasvm@gmail.com>
2024-04-23 21:14:10 +0530

    pythonexercise.py

---
+++ python-exercise.py/multiple-to-10.py
@@ -0,0 +1,10 @@
+#get input from the interface and multiply till 10
+#using for loop
+def vari():
+
+   num = int(input("Enter a number: "))
+
+   for x in range(1, 11):
+    print(num * x)
+
+vari()
---
+++ python-exercise.py/multiple-to-10_.py
@@ -0,0 +1,12 @@
+def vari():
+
+    number = int(input("Enter a number: "))  # Taking input from the user
+
+# Using a while loop to print multiples of the input number up to 10
+    i = 1
+    while i <= 10:
+      print(number * i)
+      i += 1
+
+vari()
+
