39ebf6cc89cb483b1dd5e2c996edd51a531acc49
Shrivas <Shrivasvm@gmail.com>
2024-04-24 20:27:24 +0530

    pythonlearning.py

---
+++ python-exercise.py/all-types-of-use-list.py
@@ -0,0 +1,29 @@
+#given list list = ["first","second","third"]
+num = ["first","second","third"]
+
+#add using index
+num.append["fourth"]
+print(num)
+
+#add using value
+num.append("fifth"[4])
+print(num)
+
+#delete using index 
+num.remove("first")
+print(num)
+
+#delete using value 
+num.remove[1]
+print(num)
+
+#ascending
+num_char = [9,5,10,3,1,2]
+num_char.sort()
+print(num_char)
+
+#decending
+num_char.sort( reverse = True)
+print(num_char)
+
+
