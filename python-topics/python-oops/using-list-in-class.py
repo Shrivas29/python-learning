46d5c39c67ca0e0847519982757153dc155a0272
Shrivas <Shrivasvm@gmail.com>
2024-06-01 22:25:00 +0530

    pythonlearning

--- python-topics/python-oops/using-list-in-class.py
+++ python-topics/python-oops/using-list-in-class.py
@@ -2,17 +2,20 @@ class FC:
     def __init__(self, letter="b"):
         self.letter = [letter]
 
-    def add(self):
-        self.letter.append("a")
+    def add(self,add_letter = "a"):
+        self.letter.append(add_letter)
         print("We have added a letter 'a' to the list")
 
-    def addbetween(self):
-        self.letter.insert(1, "c")
-        print("We have inserted a letter 'c' between 'b' and 'a'")
+    def addbetween(self,position,add_letter = "c"):
+        if 0 <= position <= len(self.letter):
+           self.letter.insert(position,add_letter)
+           print("We have inserted a letter 'c' between 'b' and 'a'")
+        else:
+            print(f"invalid position:{position}")
 
-    def remove(self):
-        if "a" in self.letter:
-            self.letter.remove("a")
+    def remove(self,letter_add):
+        if letter_add in self.letter:
+            self.letter.remove(letter_add)
             print("We have removed a letter 'a' from the list")
         else:
             print("The letter 'a' is not in the list to remove")
@@ -20,13 +23,13 @@ class FC:
 fc = FC()
 
 
-fc.add()
+fc.add("a")
 print(fc.letter)  
 
-fc.addbetween()
+fc.addbetween(1,"c")
 print(fc.letter) 
 
-fc.remove()
+fc.remove("a")
 print(fc.letter)  
 
     
