0cb242a88668145e62d7b63ca1efbb6cdd685e21
Shrivas <Shrivasvm@gmail.com>
2025-01-25 22:56:27 +0530

    pythonlearning

--- python-projects/blackjake-program.py
+++ python-projects/blackjake-program.py
@@ -19,30 +19,58 @@ def calculate_score(cards):
         cards.append(1)
 
         return sum(cards)
+def play_game():
 
+    user_card = []
+    computer_card = []
+    game_over = False
+    computer_score = -1
+    user_score = -1
 
-user_card = []
-computer_card = []
-game_over = False
-
-for i in range(2):
-    new_card = deal_card()
-    user_card.append(new_card)
-    computer_card.append(new_card)
-user_score = calculate_score(user_card)
-computer_score = calculate_score(computer_card)
-print(f"your cards are{user_card} adn your socre is {user_score}")
-print(f"computers cards are {computer_card} and the computer score is {computer_score}")
-
-
-if user_score == 0 or computer_score == 0 or user_score > 21:
-    game_over = True
-else:
-    extra_card = input("type 'y' to get 1 card and type 'n' if u dont want card")
-    if extra_card == "y":
+    for i in range(2):
+        new_card = deal_card()
         user_card.append(new_card)
-        
-    else:
+        computer_card.append(new_card)
+    user_score = calculate_score(user_card)
+    computer_score = calculate_score(computer_card)
+    print(f"your cards are{user_card} adn your socre is {user_score}")
+    print(f"computers cards are {computer_card} and the computer score is {computer_score}")
+
+
+    if user_score == 0 or computer_score == 0 or user_score > 21:
         game_over = True
+    else:
+        extra_card = input("type 'y' to get 1 card and type 'n' if u dont want card")
+        if extra_card == "y":
+            user_card.append(new_card)
+            
+        else:
+            game_over = True
 
 
+    while computer_score != 0 and computer_score < 17:
+        computer_card.append(deal_card())
+        computer_score = computer_score(computer_card)
+
+
+    def compare(u_score,c_score):
+        if u_score == c_score:
+            return "draw"
+        elif c_score == 0:
+            return "lost"
+        elif u_score == 0:
+            return "you win"
+        elif c_score > 21:
+            return "you win"
+        elif u_score > 21:
+            return "you lost"
+        elif u_score > c_score:
+            return "you win"
+        else: 
+            return "you lose"
+
+    compare(user_score,computer_score)
+
+while input("if you wanna countinue playing type 'y'") == "y":
+    print("/n"* 20)
+    play_game()
