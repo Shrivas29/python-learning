62f848dfc03fe3c3228d6db6d109e4b568f2d1ce
Shrivas <Shrivasvm@gmail.com>
2025-01-29 22:03:37 +0530

    pythonlearning

---
+++ python-exercise/two_sum-index.py
@@ -0,0 +1,10 @@
+## Two sum 
+## we have a list which conatins numbers
+## we need a an target variable where we store our target sum
+
+## this is an easy challenge 
+lis = [1,2,4,5,6]
+target = 11
+sum = lis[3] + lis[4]
+print(sum)
+    
