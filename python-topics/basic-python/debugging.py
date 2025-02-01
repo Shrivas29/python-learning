ac030d31b63ad543abfc007e8b33f46329a9900b
Shrivas <Shrivasvm@gmail.com>
2025-02-01 23:21:51 +0530

    pythonlearning

---
+++ python-topics/basic-python/debugging.py
@@ -0,0 +1,10 @@
+##first step-1 ---> is to indentify the error 
+
+##second step-2 ---> is to find where the error is 
+
+##third step-3 ---> try to simplyfy the error by using try/except 
+
+##fourth step-4 --> just work on it non stop until u find a solution for the error 
+
+##fifth step-5 --> after solving the code try running it to double check 
+
