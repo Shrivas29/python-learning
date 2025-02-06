a8a67920fdf0e5c7810c16f9a3b48087e61bc394
Shrivas <Shrivasvm@gmail.com>
2025-02-06 23:15:40 +0530

    pythonlearning

---
+++ python-topics/python-oops/turtel-oop.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+hello = Turtle()
+print(hello)
+hello.shape("turtel")
+hello.color("coral")
+hello.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
