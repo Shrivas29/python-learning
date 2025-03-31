0e84d4e2d46e511692f5fbf5f7b5572dbfe020e0
Shrivas <Shrivasvm@gmail.com>
2025-03-31 23:29:24 +0530

    pythonlearning

---
+++ python-exercise/reverse_string.py
@@ -0,0 +1,8 @@
+# "i am the goat"
+# exercise - make this string into a list format where the words should be in diff seperate indexes
+
+
+str = "i am the goat"
+spl = str.split()
+print(spl)
+lis = list(spl)
